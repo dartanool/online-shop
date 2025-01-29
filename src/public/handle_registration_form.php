983c505ab18c3a4ed689e3e9c89cd6b9c3a4afdd
Aleksandra <sdarganool@gmail.com>
2025-01-29 19:22:48 +0300

    Проверяет валидацию email, password

--- src/public/handle_registration_form.php
+++ src/public/handle_registration_form.php
@@ -5,7 +5,24 @@ $email = $_GET['email'];
 $password = $_GET['password'];
 $check_password = $_GET['check_password'];
 
-$pdo->exec("INSERT INTO users (name, email, password)  VALUES ('$name', '$email', '$password')");
-$statement =$pdo -> query("SELECT * FROM users ORDER BY id DESC LIMIT 1 ");
-$result = $statement-> fetch();
-print_r($result);
+function validateEmail($email) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (validateEmail($email)) {
+    if ($check_password == $password) {
+        $pdo->exec("INSERT INTO users (name, email, password)  VALUES ('$name', '$email', '$password')");
+        $statement =$pdo -> query("SELECT * FROM users ORDER BY id DESC LIMIT 1 ");
+        $result = $statement-> fetch();
+        print_r($result);
+    } else {
+        echo "Пароли не совпадают";
+    }
+} else {
+    echo "Email '$email' не валиден.";
+}
+
