43dc45332e9810dece3ded72fffc99bbdefa5c8e
Aleksandra <sdarganool@gmail.com>
2025-02-01 02:10:48 +0300

    First upgrate of validation

--- src/public/handle_registration_form.php
+++ src/public/handle_registration_form.php
@@ -1,41 +1,62 @@
 <?php
 $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname = mydb', 'user', 'pass');
-$name = $_GET['name'];
-$email = $_GET['email'];
-$password = $_GET['password'];
-$check_password = $_GET['check_password'];
+$errors = [];
 
-function validateEmail($email) {
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        return true;
-    } else {
-        echo "Email '$email' не валиден.";
+if (isset($_GET['name'])) {
+    $name = $_GET['name'];
+    $errors['name'] = validateName($name);
+} else {
+    $errors['name'] = "Name is not filled";
+}
+
+if (isset($_GET['email'])) {
+    $email = $_GET['email'];
+    $errors['email'] = validateEmail($email);
+} else {
+    $errors['email'] = "Email is not filled";
+}
+
+if (isset($_GET['password']) && isset($_GET['check_password'])) {
+    $password = $_GET['password'];
+    $check_password = $_GET['check_password'];
+    $errors['password'] = validatePassword($password, $check_password);
+} else {
+    $errors['password'] = "Password and Check_password are not filled";
+}
+
+function validateName($name) {
+    if (strlen($name) < 3) {
+        return "Name '$name' too short";
     }
 }
+
+function validateEmail($email)
+{
+    if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
+        return "Email '$email' не валиден.";
+    }
+}
+
 function validatePassword($password, $check_password)
 {
-    if ((strlen($password) > 4 || strlen($password) < 72) ||
-        (preg_match('/[A-Z]/', $password) ||
-        preg_match('/[a-z]/', $password) ||
+    if (!((strlen($password) > 4 && strlen($password) < 72) &&
+        preg_match('/[A-Z]/', $password) &&
+        preg_match('/[a-z]/', $password) &&
         preg_match('/[0-9]/', $password) ))
     {
-        if ($check_password == $password) {
-            return true;
-        } else {
-            echo "Пароли не совпадают";
-        }
-    } else {
-        echo "Некорректный пароль.
+        return "Некорректный пароль.
              Пароль должен содержать от 4 до 72 символов, хотя бы одну строчную букву, хотя бы одну заглавную букву, хотя бы одну цифру.";
+    } elseif ($check_password !== $password) {
+        return  "Пароли не совпадают";
     }
 }
 
-if (validateEmail($email)) {
-    if (validatePassword($password, $check_password)) {
+if (empty($errors)) {
         $pdo->exec("INSERT INTO users (name, email, password)  VALUES ('$name', '$email', '$password')");
         $statement =$pdo -> query("SELECT * FROM users ORDER BY id DESC LIMIT 1 ");
         $result = $statement-> fetch();
         print_r($result);
-    }
+
 }
 
+//include registration_form.php;
