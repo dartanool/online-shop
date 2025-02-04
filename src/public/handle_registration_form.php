33e6868825fadc96b4bac42ee6c1971937359f76
Aleksandra <sdarganool@gmail.com>
2025-02-04 21:13:00 +0300

    Demo version of validation

--- src/public/handle_registration_form.php
+++ src/public/handle_registration_form.php
@@ -1,87 +1,86 @@
 <?php
-$errors = [];
 
-if (isset($_POST['name'])) {
-    $name = $_POST['name'];
-    $nameError = validateName($name);
-    if ($nameError) {
-        $errors['name'] = $nameError;
+function validate(array $data)
+{
+    $errors = [];
+
+    if (isset($data['name'])) {
+        $errors['name'] = validateName($data);
+    } else {
+        $errors['name'] = "Name is not filled";
     }
-} else {
-    $errors['name'] = "Name is not filled";
+
+    if (isset($data['password']) && isset($data['check_password'])) {
+        $password = $data['password'];
+        $check_password = $data['check_password'];
+        $errors['password'] = validatePassword($data);
+    } else {
+        $errors['password'] = "Password and Check_password are not filled";
+    }
+
+    if (isset($data['email'])) {
+        $email = $data['email'];
+        $errors['email'] = validateEmail($data);
+    } else {
+        $errors['email'] = "Email is not filled";
+    }
+    return $errors;
 }
 
-
-if (isset($_POST['password']) && isset($_POST['check_password'])) {
-    $password = $_POST['password'];
-    $check_password = $_POST['check_password'];
-    $passwordError = validatePassword($password, $check_password);
-    if ($passwordError) {
-        $errors['password'] = $passwordError;
-    }
-} else {
-    $errors['password'] = "Password and Check_password are not filled";
-}
-
-if (isset($_POST['email'])) {
-    $email = $_POST['email'];
-    $emailError = validateEmail($email);
-    if ($emailError) {
-        $errors['email'] = $emailError;
-    }
-} else {
-    $errors['email'] = "Email is not filled";
-}
-
-function validateName($name) {
-    if (strlen($name) < 3) {
-        return "Name '$name' too short";
+function validateName(array $data)
+{
+    if (strlen($data['name']) < 3) {
+        return "Name {$data['name']} too short";
     } else {
         return  null;
     }
 }
 
-function validateEmail($email)
+
+function validateEmail(array $data)
 {
-    if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
-        return "Email '$email' не валиден.";
+    if (!(filter_var($data['email'], FILTER_VALIDATE_EMAIL))) {
+        return "Email {$data['email']} не валиден.";
     } else {
         $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname = mydb', 'user', 'pass');
 
         $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-        $statement->execute(['email'=>$email]);
+        $statement->execute(['email'=>$data['email']]);
 
         if (!(empty($statement->fetch()))) {
-            return "Email '$email' already exists";
+            return "Email {$data['email']} already exists";
         } else return null;
     }
 }
 
-function validatePassword($password, $check_password)
+function validatePassword(array $data)
 {
-    if (!((strlen($password) > 4 && strlen($password) < 72) &&
-        preg_match('/[A-Z]/', $password) &&
-        preg_match('/[a-z]/', $password) &&
-        preg_match('/[0-9]/', $password) ))
+    if (!((strlen($data['password']) > 4 && strlen($data['password']) < 72) &&
+        preg_match('/[A-Z]/', $data['password']) &&
+        preg_match('/[a-z]/', $data['password']) &&
+        preg_match('/[0-9]/', $data['password']) ))
     {
         return "Пароль должен содержать от 4 до 72 символов, хотя бы одну строчную букву, хотя бы одну заглавную букву, хотя бы одну цифру.";
-    } elseif ($check_password !== $password) {
+    } elseif ($data['check_password'] !== $data['password']) {
         return  "Пароли не совпадают";
     } else {
         return null;
     }
 }
+$data = $_POST;
+$errors = validate($data);
+print_r($errors);
 
 if (empty($errors)) {
     $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname = mydb', 'user', 'pass');
 
-    $password = password_hash($password, PASSWORD_DEFAULT);
+    $password = password_hash($data['password'], PASSWORD_DEFAULT);
 
     $statement = $pdo->prepare("INSERT INTO users (name, email, password)  VALUES (:name, :email, :password)");
-    $statement->execute(['name'=>$name,'email'=>$email, 'password'=>$password]);
+    $statement->execute(['name'=>$data['name'],'email'=>$data['email'], 'password'=>$data['password'],]);
 
     $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-    $statement->execute(['email'=>$email]);
+    $statement->execute(['email'=>$data['email']]);
 
     $result = $statement-> fetch();
     echo "<pre>";
