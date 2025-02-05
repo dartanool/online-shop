b5123e1da90c85a1abb47543a89baa6bc298163a
Aleksandra <sdarganool@gmail.com>
2025-02-05 19:13:05 +0300

    Update all user profile

--- src/public/handle_edit_user_profile.php
+++ src/public/handle_edit_user_profile.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+
 if  (!(isset($_SESSION['user_id']))){
     header('Location: login_form.php');
 } else {
@@ -11,12 +12,38 @@ if  (!(isset($_SESSION['user_id']))){
     $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname = mydb', 'user', 'pass');
     $statement = $pdo->query("SELECT * FROM users WHERE id = {$_SESSION['user_id']}");
     $user = $statement->fetch();
+
 // а можно ли user не находить, но чтобы в форме все равно он отображался
 
-    print_r($data);
 
+    if (isset($errors)) // или лучше empty
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
+        if (isset($name))
+        {
+            $statement = $pdo->prepare("UPDATE users SET name = :name WHERE id = {$_SESSION['user_id']}");
+            $statement->execute([':name' => $name]);
+        }
+
+        if (isset($email))
+        {
+            $statement = $pdo->prepare("UPDATE users SET email = :email WHERE id = {$_SESSION['user_id']}");
+            $statement->execute([':email' => $email]);
+        }
+
+        if (isset($password))
+        {
+            $statement = $pdo->prepare("UPDATE users SET password = :password WHERE id = {$_SESSION['user_id']}");
+            $statement->execute([':password' => $password]);
+        }
+        require_once './edit_user_profile_form.php';
+
+    }
 }
+
 
 function validate(array $data): array
 {
@@ -29,17 +56,17 @@ function validate(array $data): array
         $errors['name'] = "Name {$data['name']} too short";
     }
 
-    if (!(isset($data['password']) && isset($data['check_password'])))
-    {
-        $errors['password'] = "Password and Check_password are not filled";
-
+    if (!(isset($data['password']))) {
+        $errors['password'] = "Password is not filled";
+    } elseif (!(isset($data['checkPassword']))){
+        $errors['password'] = "Confirm password is not filled";
     } elseif (!((strlen($data['password']) > 4 && strlen($data['password']) < 72) &&
         preg_match('/[A-Z]/', $data['password']) &&
         preg_match('/[a-z]/', $data['password']) &&
         preg_match('/[0-9]/', $data['password']) ))
     {
         $errors['password'] = "Пароль должен содержать от 4 до 72 символов, хотя бы одну строчную букву, хотя бы одну заглавную букву, хотя бы одну цифру.";
-    } elseif ($data['check_password'] !== $data['password'])
+    } elseif ($data['checkPassword'] !== $data['password'])
     {
         $errors['password'] = "Пароли не совпадают";
     }
@@ -67,4 +94,3 @@ function validate(array $data): array
     return $errors;
 }
 
-require_once './edit_user_profile_form.php';
