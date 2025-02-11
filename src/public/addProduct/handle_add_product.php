33c6225e4c0bcdf22e66bf4331399476ee98715a
Aleksandra <sdarganool@gmail.com>
2025-02-11 13:54:37 +0300

    Update adding product

--- src/public/addProduct/handle_add_product.php
+++ src/public/addProduct/handle_add_product.php
@@ -6,7 +6,6 @@ if  (!(isset($_SESSION['user_id']))){
 } else {
 
     $data = $_POST;
-
     $errors = validate($data);
 
     if (empty($errors)) {
@@ -15,23 +14,37 @@ if  (!(isset($_SESSION['user_id']))){
         $amount = $_POST['amount'];
         $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname = mydb', 'user', 'pass');
 
-        $check = checksForReAdding($data, $_SESSION['user_id']);
+        $statement = $pdo->prepare("SELECT * FROM user_products WHERE product_id = :productId AND user_id =:userId");
+        $statement->execute(['productId' => $productId, 'userId' => $userId]);
 
+        $data = $statement->fetch();
 
-        if (!(empty($check['productId']))){
-            $productId = $check['productId'];
-            $newAmount = $check['amount']+$amount;
-            // update
-            $statement = $pdo->prepare("UPDATE user_products SET amount = :amount WHERE product_id = :product_id");
-            $statement->execute([':amount' => $newAmount, 'product_id' => $productId]);
-
-        } else {
-
+//        $check = checksForReAdding($data, $_SESSION['user_id']);
+        if ($data === false){
             $statement = $pdo->prepare("INSERT INTO user_products (user_id, product_id, amount) VALUES ($userId, :product_id, :amount)");
-            $statement->execute([':product_id' => $productId, ':amount' => $amount]);
-
-            $result = $statement-> fetch();
+            $statement->execute(['product_id' => $productId, 'amount' => $amount]);
+        } else {
+            $amount = $data['amount'] + $amount;
+            // update
+            $statement = $pdo->prepare("UPDATE user_products SET amount = :amount WHERE user_id =:userId and product_id = :product_id");
+            $statement->execute(['amount' => $amount, 'userId'=>$userId, 'product_id' => $productId]);
         }
+
+
+//        if (!(empty($check['productId']))){
+//            $productId = $check['productId'];
+//            $newAmount = $check['amount'] + $amount;
+//            // update
+//            $statement = $pdo->prepare("UPDATE user_products SET amount = :amount WHERE product_id = :product_id");
+//            $statement->execute([':amount' => $newAmount, 'product_id' => $productId]);
+//
+//        } else {
+//
+//            $statement = $pdo->prepare("INSERT INTO user_products (user_id, product_id, amount) VALUES ($userId, :product_id, :amount)");
+//            $statement->execute([':product_id' => $productId, ':amount' => $amount]);
+//
+//            $result = $statement-> fetch();
+//        }
 
     }
     require_once "./addProduct/add_product_form.php";
@@ -39,20 +52,27 @@ if  (!(isset($_SESSION['user_id']))){
 
 
 
-function validate(array $data) : array | string
+function validate(array $data) : array
 {
     $errors = [];
-    if (!isset($data['product_id']))
+    $productId = (int) $data['product_id'];
+    $amount = (int) $data['amount'];
+    if (!isset($productId))
     {
         $errors['productId'] = "Product id incorrect";
-    } elseif (is_int($data['product_id'])) {
-        $errors['productId'] = "Product id incorrect";
+    } else {
+        $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname = mydb', 'user', 'pass');
+        $statement = $pdo->prepare("SELECT * FROM products WHERE id = :productId");
+        $statement->execute([':productId' => $productId]);
+        $data = $statement->fetch();
+
+        if ($data === false){
+            $errors['productId'] = "Product doesn't exist";
+        }
     }
 
-    if (!isset($data['amount']))
+    if (!isset($amount))
     {
-        $errors['amount'] = "Amount incorrect";
-    } elseif (is_int($data['amount'])) {
         $errors['amount'] = "Amount incorrect";
     }
 
