0ce084bf1ed96d3a22dc8f63e00d64b332aa15e7
Aleksandra <sdarganool@gmail.com>
2025-02-12 13:14:21 +0300

    Packs Catalog into  classes

--- src/public/classes/Products.php
+++ src/public/classes/Products.php
@@ -1,6 +1,20 @@
 <?php
-
 class Products
 {
+    //Catalog
+    public function getCatalog(){
+        session_start();
 
+        if  (!(isset($_SESSION['user_id']))){
+            header('Location: login');
+        } else {
+            $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname = mydb', 'user', 'pass');
+
+            $statement = $pdo->query("SELECT * FROM products");
+
+            $products = $statement->fetchAll();
+
+            require_once "./pages/catalog_page.php";
+        }
+    }
 }
