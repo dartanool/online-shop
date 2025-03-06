1e93f2db11011a453e3db52b068f6d2d722eadb4
Александра <sdarganool@gmail.com>
2025-03-06 14:03:35 +0300

    Refactor

--- src/public/index.php
+++ src/public/index.php
@@ -13,24 +13,27 @@ $path = dirname(__DIR__);
 Autoloader::register($path);
 
 $app = new App();
-$app->get('/registration',UserController::class,'getRegistrate');
-$app->post('/registration',UserController::class,'registrate');
-$app->get('/login', UserController::class,'getLogin');
-$app->post('/login', UserController::class,'login');
-$app->get('/user-profile',UserController::class,'getProfile' );
-$app->get('/edit-user-profile', UserController::class,'getEditProfile');
-$app->post('/edit-user-profile', UserController::class,'editProfile');
-$app->get('/add-product',CartController::class,'getCatalog');
-$app->post('/add-product',CartController::class,'addProduct');
+
+
+$app->get('/user-orders', OrderController::class,'getAllOrders' );
+$app->get('/product',ProductController::class,'getProduct' );
 $app->get('/catalog', ProductController::class,'getCatalog');
 $app->get('/logout', UserController::class,'logout' );
 $app->get('/cart', CartController::class,'getCart' );
 $app->get('/create-order',OrderController::class,'getCreateForm' );
+$app->get('/add-product',CartController::class,'getCatalog');
+$app->get('/user-profile',UserController::class,'getProfile' );
+$app->get('/edit-user-profile', UserController::class,'getEditProfile');
+$app->get('/login', UserController::class,'getLogin');
+$app->get('/registration',UserController::class,'getRegistrate');
+
+$app->post('/registration',UserController::class,'registrate');
+$app->post('/login', UserController::class,'login');
+$app->post('/edit-user-profile', UserController::class,'editProfile');
+$app->post('/add-product',CartController::class,'addProduct');
 $app->post('/create-order',OrderController::class,'create' );
-$app->get('/user-orders', OrderController::class,'getAllOrders' );
 $app->post('/decrease-product',CartController::class,'decreaseProduct' );
 $app->post('/product',ProductController::class,'getProduct' );
-$app->get('/product',ProductController::class,'getProduct' );
 $app->post('/add-review',ProductController::class,'addReview' );
 
 $app->run();
